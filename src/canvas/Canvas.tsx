8523181be566bfcfcbde69e57195a679220a8929
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-02-14 22:56:36 +0100

    add UnitTest for Canvas

--- src/canvas/Canvas.tsx
+++ src/canvas/Canvas.tsx
@@ -1,16 +1,27 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useEffect } from 'react';
 import { SVG } from '@svgdotjs/svg.js';
 
 interface CanvasProps {
     className?: string;
 }
 
-const draw = SVG().addTo('body').size('100vw', '100vh');
+const draw = SVG().addTo('body').size('100vw', '100vh').id('canvas');
 
 const Canvas: FunctionComponent<CanvasProps> = (props) => {
     draw.rect(200, 300).move(50, 25).fill('#f06');
+
+    useEffect(() => {
+        draw.node.addEventListener('click', onCanvasClick);
+        return () => {
+            draw.node.removeEventListener('click', onCanvasClick);
+        };
+    }, []);
 
     return <></>;
 };
 
 export default Canvas;
+
+const onCanvasClick = (ev: MouseEvent) => {
+    console.log('clicked in ', draw.node, ' in position', ev.offsetX, ev.offsetY);
+};
