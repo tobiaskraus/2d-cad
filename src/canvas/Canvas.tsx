74b0390c485ec674c65687af49faf9493eff3bb0
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-02-17 19:27:58 +0100

    add grid (small: 1 unit, big: 10)
    - big: thicker line
    - renders correnct when view moves & zooms
    - but very close zoom doesn't show grid anymore

--- src/canvas/Canvas.tsx
+++ src/canvas/Canvas.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent, useState } from 'react';
+import Grid from './components/Grid';
 import ViewBoxControl, { ViewBox } from './components/ViewBoxControl';
 
 interface CanvasProps {
@@ -6,23 +7,29 @@ interface CanvasProps {
 }
 
 const Canvas: FunctionComponent<CanvasProps> = (props) => {
-    const [viewBox, setViewBox] = useState<ViewBox>({ x: 0, y: 0, width: 300, height: 250 });
+    const [viewBox, setViewBox] = useState<ViewBox>({ x: 0, y: 0, width: 80, height: 60 });
 
     console.log('viewBox', viewBox);
     return (
         <div>
             <ViewBoxControl value={viewBox} onChange={setViewBox} />
             <svg
+                preserveAspectRatio="xMidYMid slice"
                 viewBox={`${viewBox.x} ${viewBox.y} ${viewBox.width} ${viewBox.height}`}
                 xmlns="http://www.w3.org/2000/svg"
+                width="500"
+                height="500"
                 style={{
                     height: '100vh',
                     width: '100vw',
                 }}
             >
+                <Grid viewBox={viewBox} />
                 <rect
-                    width="100"
-                    height="70"
+                    x="10"
+                    y="20"
+                    width="10"
+                    height="20"
                     style={{ fill: 'rgb(0,0,255);stroke-width:10;stroke:rgb(0,0,0)' }}
                 />
             </svg>
---
+++ src/canvas/components/Grid.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent } from 'react';
+import { ViewBox } from './ViewBoxControl';
+
+interface GridProps {
+    viewBox: ViewBox;
+}
+
+const Grid: FunctionComponent<GridProps> = (props) => {
+    const strokeWidth = ((props.viewBox.width + props.viewBox.height) / 2) * 0.001;
+    return (
+        <>
+            <defs>
+                <pattern id="smallGrid" width="1" height="1" patternUnits="userSpaceOnUse">
+                    <path
+                        d="M 1 0 L 0 0 0 1"
+                        fill="none"
+                        stroke="gray"
+                        stroke-width={strokeWidth}
+                    />
+                </pattern>
+                <pattern id="grid" width="10" height="10" patternUnits="userSpaceOnUse">
+                    <rect width="10" height="10" fill="url(#smallGrid)" />
+                    <path
+                        d="M 10 0 L 0 0 0 10"
+                        fill="none"
+                        stroke="gray"
+                        stroke-width={strokeWidth * 2}
+                    />
+                </pattern>
+            </defs>
+            <rect
+                x={props.viewBox.x}
+                y={props.viewBox.y}
+                width={props.viewBox.width}
+                height={props.viewBox.height}
+                fill="url(#grid)"
+            />
+        </>
+    );
+};
+
+export default Grid;
