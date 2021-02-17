9aa51523fb70dc222c0c62dda51451ea68b07dcb
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-02-17 22:49:31 +0100

    fix small things in Canvas

--- src/canvas/Canvas.tsx
+++ src/canvas/Canvas.tsx
@@ -2,14 +2,9 @@ import React, { FunctionComponent, useState } from 'react';
 import Grid from './components/grid/Grid';
 import ViewBoxControl, { ViewBox } from './components/ViewBoxControl';
 
-interface CanvasProps {
-    className?: string;
-}
-
-const Canvas: FunctionComponent<CanvasProps> = (props) => {
+const Canvas: FunctionComponent = () => {
     const [viewBox, setViewBox] = useState<ViewBox>({ x: 0, y: 0, width: 80, height: 60 });
 
-    console.log('viewBox', viewBox);
     return (
         <div>
             <ViewBoxControl value={viewBox} onChange={setViewBox} />
@@ -30,7 +25,7 @@ const Canvas: FunctionComponent<CanvasProps> = (props) => {
                     y="20"
                     width="10"
                     height="20"
-                    style={{ fill: 'rgb(0,0,255);stroke-width:10;stroke:rgb(0,0,0)' }}
+                    style={{ fill: 'rgb(0,0,255)', strokeWidth: 0.2, stroke: 'rgb(0,0,0)' }}
                 />
             </svg>
         </div>
