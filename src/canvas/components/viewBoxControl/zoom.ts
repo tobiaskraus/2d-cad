883eeba418ea65729433fa31e4b5dfdce0a45c5e
Tobias Kraus (Lenovo) <contact@tobias-kraus.com>
2021-06-13 21:57:13 +0200

    refactor zoom.ts: kill redundancy

--- src/canvas/components/viewBoxControl/zoom.ts
+++ src/canvas/components/viewBoxControl/zoom.ts
@@ -4,25 +4,24 @@ import { ViewBox } from './ViewBox';
 export const zoomFactor = 1.2;
 
 export function zoomIn(vb: ViewBox): ViewBox {
-    const width = vb.width / zoomFactor;
-    const height = vb.height / zoomFactor;
-    return {
-        x: vb.x + (vb.width - width) * 0.5,
-        y: vb.y + (vb.height - height) * 0.5,
-        width,
-        height,
-        pixelsPerUnit: vb.pixelsPerUnit / zoomFactor,
-    };
+    return zoom(vb, 1 / zoomFactor);
 }
 
 export function zoomOut(vb: ViewBox) {
-    const width = vb.width * zoomFactor;
-    const height = vb.height * zoomFactor;
+    return zoom(vb, zoomFactor);
+}
+
+/**
+ * @param reverseZoomFactor 0-1: zoomIn / more than 1: zoomOut
+ */
+function zoom(vb: ViewBox, reverseZoomFactor: number) {
+    const width = vb.width * reverseZoomFactor;
+    const height = vb.height * reverseZoomFactor;
     return {
         x: vb.x + (vb.width - width) * 0.5,
         y: vb.y + (vb.height - height) * 0.5,
-        width: vb.width * zoomFactor,
-        height: vb.height * zoomFactor,
-        pixelsPerUnit: vb.pixelsPerUnit * zoomFactor,
+        width: vb.width * reverseZoomFactor,
+        height: vb.height * reverseZoomFactor,
+        pixelsPerUnit: vb.pixelsPerUnit * reverseZoomFactor,
     };
 }
